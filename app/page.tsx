c25b3bb25579f1f15575aac5eccb831dd4251082
abhishekas7 <abhisheksubash7.as@gmail.com>
2026-09-24 12:02:42 +0530

    installed next js and settup folder structures

---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+
+
+export default function HomePage() {
+  return (
+    <main>
+      <h1>Our Products</h1>
+
+      <div className="flex gap-6 overflow-x-auto yes">
+dddd
+      </div>
+    </main>
+  );
+}
---
+++ app/product/page.tsx
@@ -0,0 +1,10 @@
+
+export default function Product() {
+  return (
+    <div className="flex min-h-screen justify-center bg-red">
+
+      
+Product 
+    </div>
+  );
+}
